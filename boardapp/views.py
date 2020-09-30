d33ec488d588270ceeafbe6f28c4558b0cc2d053
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-09-30 23:56:52 +0900

    edit signupfunc in views

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def signupfunc(request):
     if request.method == 'POST':
-        print('this is post method')
-    else:
-        print('this is not post method')
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username, '', password)
+        #print(request.POST) postをターミナルで確認できる
+        return render(request, 'signup.html', {'some':100})
     #renderメソッド
     return render(request, 'signup.html', {'some':100})
