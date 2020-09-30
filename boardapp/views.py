9746a41dc30f06e8ae7f718afa7b97c5a7f5fab0
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-10-01 00:14:38 +0900

    add User.objects

--- boardapp/views.py
+++ boardapp/views.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 # Create your views here.
 
 def signupfunc(request):
+    #ターミナルでQuerySetの確認。
+    #user2 = User.objects.all()
+    user2 = User.objects.get(username='yoshi')
+    print(user2)
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
