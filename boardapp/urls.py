ea908eccd9cdee7b22a2c1a9a766bb16abdc69f8
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-10-01 21:52:24 +0900

    add name

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import signupfunc
+from .views import signupfunc, loginfunc
 
 urlpatterns = [
-    path('signup/', signupfunc),
+    path('signup/', signupfunc, name='signup'),
+    path('login/', loginfunc, name='login'),
 ]
